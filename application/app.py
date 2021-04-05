e26bd8a1461d89bfec6402cf66949e0ce8166280
audran <audranthomas69@gmail.com>
2021-04-05 16:49:46 +0200

    redirection in case not blacklist

--- application/app.py
+++ application/app.py
@@ -33,6 +33,7 @@ def my_token_in_blacklist_callback(jwt_payload):
     jti = jwt_payload["jti"]
     if jti_in_blacklist(jti):
         return redirect(url_for('auth.login'))
+    return redirect(url_for('main.gallery'))
 
 
 @jwt.unauthorized_loader
